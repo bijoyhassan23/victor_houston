97afdb3d5cdbd4bede91b9ccd462be37ab9827af
Md. Rakibul Islam <raaakib0@gmail.com>
2026-01-15 14:50:43 +0600

    testimonial slider design 4th

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,60 @@ document.addEventListener('click', e=>{
     dropdown.classList.remove('open');
   }
 });
+
+// testimonial slider
+// const slides = document.querySelectorAll('.slide');
+// const dots = document.querySelectorAll('.dots span');
+// let index = 0;
+
+// function showSlide(i){
+//   slides.forEach(s=>s.classList.remove('active'));
+//   dots.forEach(d=>d.classList.remove('active'));
+//   slides[i].classList.add('active');
+//   dots[i].classList.add('active');
+// }
+
+// document.querySelector('.next').onclick = ()=>{
+//   index = (index + 1) % slides.length;
+//   showSlide(index);
+// };
+
+// document.querySelector('.prev').onclick = ()=>{
+//   index = (index - 1 + slides.length) % slides.length;
+//   showSlide(index);
+// };
+
+// dots.forEach((dot,i)=>{
+//   dot.onclick = ()=>{
+//     index = i;
+//     showSlide(i);
+//   };
+// });
+
+// const slides = document.querySelector('.slides');
+// const dots = document.querySelectorAll('.dots span');
+// let index = 0;
+
+// function showSlide(i){
+//   slides.style.transform = `translateX(-${i * 100}%)`;
+
+//   dots.forEach(d => d.classList.remove('active'));
+//   dots[i].classList.add('active');
+// }
+
+// document.querySelector('.next').onclick = () => {
+//   index = (index + 1) % dots.length;
+//   showSlide(index);
+// };
+
+// document.querySelector('.prev').onclick = () => {
+//   index = (index - 1 + dots.length) % dots.length;
+//   showSlide(index);
+// };
+
+// dots.forEach((dot, i) => {
+//   dot.onclick = () => {
+//     index = i;
+//     showSlide(i);
+//   };
+// });
